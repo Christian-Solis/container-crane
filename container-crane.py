6f6645411b3846c6a979a6fc7098cee56ecc4c7f
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-02-16 14:16:43 -0600

    Start of challenge
    Challenge Specification

---
+++ container-crane.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# Christian Ricardo Solís Cortés
+# A01063685
+# Artificial Intelligence
+# Assignment: (Un)informed Search Lab
+# The Container Crane Problem
+# -----------------------------------------------------------------------------
