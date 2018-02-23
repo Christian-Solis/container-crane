1057242f0a6263628cecb70e727d8c8c4fd836d3
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-02-23 16:40:09 -0600

    Run
    run file

--- container-crane.py
+++ container-crane.py
@@ -6,10 +6,18 @@
 # The Container Crane Problem
 # -----------------------------------------------------------------------------
 
+# Movements with their cost
 pick = 0.5
 move = 1
 put = 0.5
 
+# Queue
+queue = [()]
+
+# Visited items
+visited = {0}
+
+# Inputs
 print("\n Give me the max height of a stack: ")
 height = input()
 print("\n Give me the initial location of containers:")
@@ -20,6 +28,7 @@ print("\n Example (A, C); X; X \n")
 goal = input()
 print("\n")
 
+
 initial_list = initial.split(";")
 goal_list = goal.split(";")
 
@@ -27,6 +36,30 @@ stack0 = initial_list[0]
 stack1 = initial_list[1]
 stack2 = initial_list[2]
 
-print(stack0)
-print(stack1)
-print(stack2)
+# print(stack0)
+# print(stack1)
+# print(stack2)
+
+class Node:
+    def __init__(Parent, Action, Path_Cost, State):
+
+def Action():
+
+def Prev_Path_Cost:
+
+def Prev_State:
+
+
+def Path_cost(Prev_Path_Cost):
+    ptc = Prev_Path_Cost + pick + move + put
+
+def State(Prev_State):
+    st = [stack0, stack1, stack2]
+
+# -----------------------------------------------------------------------------
+# Uniform Cost Search
+# -----------------------------------------------------------------------------
+
+def ucs(Node, height, stack0, stack1, stack2, goal):
+    queue = [()]
+    visited = {}
